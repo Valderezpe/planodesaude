e1a174766e7041bd43199393f2fb55bbfdd61212
valderez <valderezpe@gmail.com>
2024-08-05 11:06:06 -0300

    add: feat create project ,structure

---
+++ src/main/java/com/planosaude/planodesaude/PlanodesaudeApplication.java
@@ -0,0 +1,13 @@
+package com.planosaude.planodesaude;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanodesaudeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanodesaudeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/planosaude/planodesaude/PlanodesaudeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.planosaude.planodesaude;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanodesaudeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
