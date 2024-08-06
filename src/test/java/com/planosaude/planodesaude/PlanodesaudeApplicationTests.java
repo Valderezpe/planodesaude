f7c9d41776fc3f79836b9e96203f740980b5510d
valderez <valderezpe@gmail.com>
2024-08-06 10:38:27 -0300

    feat: add resource, properties testes Automatizados

--- src/test/java/com/planosaude/planodesaude/PlanodesaudeApplicationTests.java
+++ src/test/java/com/planosaude/planodesaude/PlanodesaudeApplicationTests.java
@@ -1,13 +1,41 @@
 package com.planosaude.planodesaude;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+import com.planosaude.planodesaude.entity.plano;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class PlanodesaudeApplicationTests {
+	@Autowired
+	private WebTestClient webTestClient;
 
 	@Test
-	void contextLoads() {
+	void testCreateplanoSuccess() {
+	var plano = new plano("plano 1", "nome plano 1", "telefone plano 1", "dataNascimento", "dataInclusao", "dataAtualizacao",false, 1);
+
+	webTestClient
+	.post()
+	.uri("/planos")
+	.bodyValue(plano)
+	.exchange()
+	.expectStatus().isOk()
+	.expectBody()
+	.jsonPath("$").isArray()
+	.jsonPath("$.length()").isEqualTo(1)
+	.jsonPath("$[0].nome").isEqualTo(plano.getNome())
+	.jsonPath("$[0].nome").isEqualTo(plano.getTelefone())
+	.jsonPath("$[0].nome").isEqualTo(plano.getDataNascimento())
+	.jsonPath("$[0].nome").isEqualTo(plano.getDataInclusao())
+	.jsonPath("$[0].nome").isEqualTo(plano.getDataAtualizacao());
+	
+	}
+
+	@Test
+	void testCreatePlanoSaudeFailure() {
 	}
 
 }
